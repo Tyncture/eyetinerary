8d7c6d4b37c1906a6649f8fc3f0091c294d6fc74
John Su <git@tyncture.dev>
2019-06-14 22:32:33 +0100

    Sidebar structure, layout and design

---
+++ src/pages/components/base/sidebar.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./sidebar.scss";
+
+class Sidebar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="sidebar">
+        <header className="sidebar-header" />
+        <div className="sidebar-content">
+          {this.props.children}
+        </div>
+        <footer className="sidebar-footer" />
+      </div>
+    );
+  }
+}
+
+export default Sidebar;
