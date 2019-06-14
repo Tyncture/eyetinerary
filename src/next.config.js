eb5695ce4982a52ed7d744f9ac7493f771620d55
John Su <git@tyncture.dev>
2019-06-14 17:56:24 +0100

    Configure for Next.js

---
+++ src/next.config.js
@@ -0,0 +1,5 @@
+// As per https://github.com/zeit/next-plugins/tree/master/packages/next-typescript
+const withTypescript = require('@zeit/next-typescript')
+module.exports = withTypescript({
+  /* config options here */
+})
---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+export class IndexPage extends Component {
+  render() {
+    return (
+      <main>
+        Next.js test.
+      </main>
+    )
+  }
+}
+
+export default IndexPage;
