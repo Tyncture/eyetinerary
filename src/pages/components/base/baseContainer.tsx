cb5dc907f708e9b2f05ecc99fc5e984b1b8ac951
John Su <git@tyncture.dev>
2019-06-14 22:31:34 +0100

    Base container restructuring

---
+++ src/pages/components/base/baseContainer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./baseContainer.scss";
+class BaseContainer extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div className="base-container">{this.props.children}</div>;
+  }
+}
+
+export default BaseContainer;
