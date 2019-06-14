a48f58d2b0eb6764c08ff5fb4674b8228a383d78
John Su <git@tyncture.dev>
2019-06-14 20:05:59 +0100

    Global app SCSS with Normalize

---
+++ src/pages/_app.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import App, { Container } from "next/app";
+import "./styles/app.scss";
+
+class CustomApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {};
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+
+    return { pageProps };
+  }
+
+  render() {
+    const { Component, pageProps } = this.props;
+
+    return (
+      <Container>
+        <Component {...pageProps} className="app-container"/>
+      </Container>
+    );
+  }
+}
+
+export default CustomApp;
