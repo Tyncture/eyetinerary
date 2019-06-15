155b05430613ddff5044930683dbf83c5aa88261
John Su <git@tyncture.dev>
2019-06-15 01:12:25 +0100

    ItineraryHeader component and style changes

---
+++ src/pages/components/itinerary/itineraryHeader.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faGlobeEurope,
+  faCalendarAlt,
+  faClock
+} from "@fortawesome/free-solid-svg-icons";
+import "./itineraryHeader.scss";
+
+interface IProps {
+  title: string;
+  description?: string;
+  location?: string;
+  countryCode?: string;
+}
+
+class ItineraryHeader extends React.Component<IProps> {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="itinerary-header">
+        <h1>{this.props.title}</h1>
+        <h2>{this.props.description}</h2>
+        <div className="itinerary-header-info">
+          <div className="itinerary-header-info-group">
+            <div>
+              <FontAwesomeIcon icon={faGlobeEurope} />
+            </div>
+            <div>
+              Destination: {this.props.location}, {this.props.countryCode}
+            </div>
+          </div>
+          <div className="itinerary-header-info-group">
+            <div>
+              <FontAwesomeIcon icon={faCalendarAlt} />
+            </div>
+            <div>Trip Duration: 3 days</div>
+          </div>
+        </div>
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+export default ItineraryHeader;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,13 +3,19 @@ import BaseContainer from "./components/base/baseContainer";
 import "./index.scss";
 import Sidebar from "./components/base/sidebar";
 import Main from "./components/base/main";
+import ItineraryHeader from "./components/itinerary/itineraryHeader";
 
 export class IndexPage extends Component {
   render() {
     return (
       <BaseContainer>
           <Sidebar />
-          <Main />
+          <Main>
+            <ItineraryHeader title="This thing works and will continue to work"
+            description="This thing works and will continue to work"
+            location="Bangkok"
+            countryCode="Thailand"/>
+          </Main>
         </BaseContainer>
     );
   }
