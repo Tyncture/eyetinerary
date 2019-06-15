fe1b9da26e75ac1aaa4daa280cee6b2e3eaf3800
John Su <git@tyncture.dev>
2019-06-15 15:06:08 +0100

    Conditional rendering based on ID and API response for Itinerary component

--- src/pages/itinerary.tsx
+++ src/pages/itinerary.tsx
@@ -5,6 +5,7 @@ import Main from "../components/base/main";
 import ItineraryHeader from "../components/itinerary/itineraryHeader";
 import ItineraryPageList from "../components/itinerary/itineraryPageList";
 import { IItinerary } from "../components/itinerary/types";
+import validator from "validator";
 import "./itinerary.scss";
 
 interface IProps {
@@ -16,6 +17,8 @@ interface IProps {
 
 interface IState {
   itinerary: IItinerary;
+  apiErrorCode: number;
+  invalidIdFormat: boolean;
 }
 
 class Itinerary extends React.Component<IProps, IState> {
@@ -26,19 +29,49 @@ class Itinerary extends React.Component<IProps, IState> {
   constructor(props) {
     super(props);
     this.state = {
-      itinerary: null
+      itinerary: null,
+      apiErrorCode: 200,
+      invalidIdFormat: false
     };
   }
 
   async componentDidMount() {
-    this.setState({
-      itinerary: await this.getItinerary(this.props.query.id)
-    });
+    await this.bootstrapState();
   }
 
-  async getItinerary(id: number | string): Promise<IItinerary> {
-    const response = await fetch(`${process.env.EYET_API}/itinerary/${id}`);
-    return response.status === 200 ? await response.json() : null;
+  async componentDidUpdate(prevProps) {
+    if (this.props.query !== prevProps.query) {
+      await this.bootstrapState();
+    }
+  }
+
+  async bootstrapState() {
+    const validIdFormat =
+      this.props.query.id && validator.isNumeric(this.props.query.id);
+    if (validIdFormat) {
+      this.setState({ invalidIdFormat: false });
+      await this.loadItinerary();
+    } else {
+      this.setState({ invalidIdFormat: true });
+    }
+  }
+
+  async loadItinerary() {
+    try {
+      const response = await fetch(
+        `${process.env.EYET_API}/itinerary/${this.props.query.id}`
+      );
+      const httpStatus = response.status;
+      const json = await response.json();
+      if (httpStatus === 200) {
+        this.setState({ itinerary: json, apiErrorCode: 200 });
+      } else {
+        this.setState({ apiErrorCode: httpStatus });
+        console.log(json);
+      }
+    } catch (e) {
+      this.setState({ apiErrorCode: -1 });
+    }
   }
 
   render() {
@@ -46,15 +79,50 @@ class Itinerary extends React.Component<IProps, IState> {
       <BaseContainer>
         <Sidebar />
         <Main>
-          {this.state.itinerary && (
-            <div className="itinerary">
-              <ItineraryHeader
-                title={this.state.itinerary.title}
-                description="This thing works and will continue to work"
-                location="Bangkok"
-                countryCode="Thailand"
-              />
-              <ItineraryPageList pages={this.state.itinerary.pages} />
+          {this.props.query.id && !this.state.invalidIdFormat && (
+            <div>
+              {this.state.itinerary && (
+                <div className="itinerary">
+                  <ItineraryHeader
+                    title={this.state.itinerary.title}
+                    description="This thing works and will continue to work"
+                    location="Bangkok"
+                    countryCode="Thailand"
+                  />
+                  <ItineraryPageList pages={this.state.itinerary.pages} />
+                </div>
+              )}
+              {this.state.apiErrorCode === 404 && (
+                <div>
+                  <h1>Itinerary Not Found</h1>
+                  <p>
+                    The itinerary that you requested cannot be found. It may
+                    have been deleted if it was previously accessible.
+                  </p>
+                </div>
+              )}
+              {this.state.apiErrorCode !== 200 &&
+                this.state.apiErrorCode !== 404 && (
+                  <div>
+                    <h1>Server Communication Error</h1>
+                    <p>
+                      Something happened. And it probably wasn't what you
+                      expected.
+                    </p>
+                    <p>
+                      <i>HTTP Status Code: {this.state.apiErrorCode}</i>
+                    </p>
+                  </div>
+                )}
+            </div>
+          )}
+          {(!this.props.query.id || this.state.invalidIdFormat) && (
+            <div>
+              <h1>Itinerary ID Missing</h1>
+              <p>
+                The itinerary ID is missing or invalid. Please specify the
+                itinerary ID as a query parameter in the URL.
+              </p>
             </div>
           )}
         </Main>
