c652d51c15d6af94e5b8f8e5891bd16fab14fe59
John Su <git@tyncture.dev>
2019-06-15 15:19:09 +0100

    Global fallback head title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,17 @@
-import { Component } from 'react';
-import BaseContainer from '../components/base/baseContainer';
-import Sidebar from '../components/base/sidebar';
-import Main from '../components/base/main';
-import './index.scss';
+import { Component } from "react";
+import BaseContainer from "../components/base/baseContainer";
+import Sidebar from "../components/base/sidebar";
+import Main from "../components/base/main";
+import Head from "next/head";
+import "./index.scss";
 
 export class IndexPage extends Component {
   render() {
     return (
       <BaseContainer>
+        <Head>
+          <title>Eyetinerary</title>
+        </Head>
         <Sidebar />
         <Main>
           <div>Home</div>
