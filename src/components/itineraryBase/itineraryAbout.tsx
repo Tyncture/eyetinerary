e4d05a8ead4ea4853495c67f54f4f1034be648a0
John Su <git@tyncture.dev>
2019-06-17 02:11:04 +0100

    Fix to prevent fetch response triggering setState warning when unmounted

--- src/components/itineraryBase/itineraryAbout.tsx
+++ src/components/itineraryBase/itineraryAbout.tsx
@@ -27,13 +27,19 @@ const initialState: IState = {
 };
 
 class ItineraryAbout extends React.Component<IProps, IState> {
+  private _mounted: boolean;
   constructor(props: IProps) {
     super(props);
     this.state = initialState;
   }
 
   async componentDidMount() {
+    this._mounted = true;
     await this.bootstrapState();
+  }
+
+  componentWillUnmount() {
+    this._mounted = false;
   }
 
   async componentDidUpdate(prevProps: IProps) {
@@ -55,10 +61,12 @@ class ItineraryAbout extends React.Component<IProps, IState> {
           `${process.env.EYET_API}/user/${this.state.authorId}`
         );
         const json = await response.json();
-        if (response.status === 200) {
-          this.setState({ author: json });
-        } else {
-          console.log(json);
+        if (this._mounted) {
+          if (response.status === 200) {
+            this.setState({ author: json });
+          } else {
+            console.log(json);
+          }
         }
       }
     } catch (e) {
