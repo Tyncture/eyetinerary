5f35428b6e8a8bba1eb81ab5e2e90b384031fdab
John Su <git@tyncture.dev>
2019-06-19 07:56:20 +0100

    Base CreateItinerary and EditItinerary components

---
+++ src/pages/createItinerary.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import BaseContainer from '../components/base/baseContainer';
+import Sidebar from '../components/base/sidebar';
+import Main from '../components/base/main';
+
+class CreateItinerary extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <BaseContainer>
+        <Sidebar />
+        <Main>Placeholder</Main>
+      </BaseContainer>
+    );
+  }
+}
+
+export default CreateItinerary;
