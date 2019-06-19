c22880b8f2dc41bf0deaeb442f96242e83396888
John Su <git@tyncture.dev>
2019-06-19 03:05:27 +0100

    Constrain user location length to 50 chars

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -4,7 +4,57 @@ import Sidebar from "../components/base/sidebar";
 import Main from "../components/base/main";
 import "./register.scss";
 
-class Register extends React.Component {
+interface IState {
+  username: string;
+  password: string;
+  email: string;
+  location: string;
+}
+
+const initialState: IState = {
+  username: "",
+  password: "",
+  email: "",
+  location: ""
+};
+
+class Register extends React.Component<any, IState> {
+  constructor(props) {
+    super(props);
+    this.state = initialState;
+    this.handleUsernameChange = this.handleUsernameChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handleLocationChange = this.handleLocationChange.bind(this);
+  }
+
+  handleUsernameChange(e: React.ChangeEvent<HTMLInputElement>) {
+    this.setState({ username: e.target.value });
+  }
+
+  handleUsernameKeyDown(e: React.KeyboardEvent<HTMLInputElement>) {
+    if (!e.key.match(/[a-zA-Z0-9-_]/)) {
+      e.preventDefault();
+    }
+  }
+
+  handlePasswordChange(e: React.ChangeEvent<HTMLInputElement>) {
+    this.setState({ password: e.target.value });
+  }
+  handleEmailChange(e: React.ChangeEvent<HTMLInputElement>) {
+    this.setState({ email: e.target.value });
+  }
+
+  handleEmailKeyDown(e: React.KeyboardEvent<HTMLInputElement>) {
+    if (!e.key.match(/[a-zA-Z0-9-_@\.]/)) {
+      e.preventDefault();
+    }
+  }
+
+  handleLocationChange(e: React.ChangeEvent<HTMLInputElement>) {
+    this.setState({ location: e.target.value });
+  }
+
   render() {
     return (
       <BaseContainer>
@@ -22,6 +72,9 @@ class Register extends React.Component {
                     type="text"
                     id="register-form-username"
                     name="register-form-username"
+                    value={this.state.username}
+                    onChange={this.handleUsernameChange}
+                    onKeyDown={this.handleUsernameKeyDown}
                     placeholder="Username"
                     required={true}
                     maxLength={50}
@@ -33,6 +86,8 @@ class Register extends React.Component {
                     type="password"
                     id="register-form-password"
                     name="register-form-password"
+                    value={this.state.password}
+                    onChange={this.handlePasswordChange}
                     placeholder="Password"
                     required={true}
                     maxLength={72}
@@ -47,7 +102,11 @@ class Register extends React.Component {
                     type="text"
                     id="register-form-email"
                     name="register-form-email"
+                    value={this.state.email}
+                    onChange={this.handleEmailChange}
+                    onKeyDown={this.handleEmailKeyDown}
                     placeholder="example@outlook.com"
+                    maxLength={140}
                   />
                 </div>
                 <div className="register-form--group">
@@ -59,8 +118,10 @@ class Register extends React.Component {
                     type="text"
                     id="register-form-location"
                     name="register-form-location"
+                    value={this.state.location}
+                    onChange={this.handleLocationChange}
                     placeholder="Bangkok, Thailand"
-                    maxLength={20}
+                    maxLength={50}
                   />
                 </div>
                 <div className="register-form--group">
