f81ea404984f3d79ee7facc23c70f658a146cdc3
John Su <git@tyncture.dev>
2019-06-19 23:33:34 +0100

    Base createItinerary component template

--- src/pages/createItinerary.tsx
+++ src/pages/createItinerary.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import BaseContainer from '../components/base/baseContainer';
-import Sidebar from '../components/base/sidebar';
-import Main from '../components/base/main';
+import React from "react";
+import BaseContainer from "../components/base/baseContainer";
+import Sidebar from "../components/base/sidebar";
+import Main from "../components/base/main";
 
 class CreateItinerary extends React.Component {
   constructor(props) {
@@ -12,7 +12,34 @@ class CreateItinerary extends React.Component {
     return (
       <BaseContainer>
         <Sidebar />
-        <Main>Placeholder</Main>
+        <Main>
+          <div className="create-itinerary">
+            <header className="create-itinerary-header">
+              <h1>Create Itinerary</h1>
+            </header>
+            <main>
+              <section>
+                <header>
+                  <h2>Itinerary</h2>
+                </header>
+                <div>
+                  <label>Let's give your itinerary a name. What do you want to call it?</label>
+                </div>
+              </section>
+              <section>
+                <header>
+                  <h2>Pages</h2>
+                </header>
+                <div>
+                  <label>
+                    Now let's add some pages to your itinerary. Use a page to
+                    represent a single day or whatever unit of time you prefer.
+                  </label>
+                </div>
+              </section>
+            </main>
+          </div>
+        </Main>
       </BaseContainer>
     );
   }
