6785511487fa82ccd5bd4c98b981193f263cdb57
John Su <git@tyncture.dev>
2019-06-20 10:22:19 +0100

    Action creatore for user store in Redux

---
+++ src/store/user/actions.ts
@@ -0,0 +1,34 @@
+import {
+  CLEAR_USER,
+  SET_USERNAME,
+  SET_USER_ID,
+  SET_USER_TOKEN,
+  UserActionTypes
+} from "./types";
+
+export function setUserId(id: number): UserActionTypes {
+  return {
+    type: SET_USER_ID,
+    id
+  };
+}
+
+export function setUsername(username: string): UserActionTypes {
+  return {
+    type: SET_USERNAME,
+    username
+  };
+}
+
+export function setToken(token: string): UserActionTypes {
+  return {
+    type: SET_USER_TOKEN,
+    token
+  };
+}
+
+export function clearUser(): UserActionTypes {
+  return {
+    type: CLEAR_USER
+  };
+}
