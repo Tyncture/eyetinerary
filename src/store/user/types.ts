337e5a152b718ef16d686e74e13b2bf57a234182
John Su <git@tyncture.dev>
2019-06-20 10:21:22 +0100

    TypeScript definitions for Redux store models, actions and types

---
+++ src/store/user/types.ts
@@ -0,0 +1,35 @@
+export const SET_USER_ID = "SET_USER_ID";
+export const SET_USERNAME = "SET_USERNAME";
+export const SET_USER_TOKEN = "SET_USER_TOKEN";
+export const CLEAR_USER = "CLEAR_USER";
+
+interface ISetUserIdAction {
+  type: typeof SET_USER_ID;
+  id: number;
+}
+
+interface ISetUsernameAction {
+  type: typeof SET_USERNAME;
+  username: string;
+}
+
+interface ISetUserTokenAction {
+  type: typeof SET_USER_TOKEN;
+  token: string;
+}
+
+interface IClearUserAction {
+  type: typeof CLEAR_USER;
+}
+
+export type UserActionTypes =
+  | ISetUserIdAction
+  | ISetUsernameAction
+  | ISetUserTokenAction
+  | IClearUserAction;
+
+export interface IUser {
+  id: number;
+  username: string;
+  token: string;
+}
