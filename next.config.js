892369cf7f66167dcf7894eac48158a1627f00ab
John Su <git@tyncture.dev>
2019-07-23 22:12:46 +0100

    Implement workaround for zeit/next-plugins issue 392 and remove withTypescript (redundant in Next.js 9)

--- next.config.js
+++ next.config.js
@@ -1,18 +1,36 @@
-// As per https://github.com/zeit/next-plugins/tree/master/packages/next-typescript
-const withTypescript = require('@zeit/next-typescript');
-const withSass = require('@zeit/next-sass');
-const nextEnv = require('next-env');
-const dotenvLoad = require('dotenv-load');
+const withSass = require("@zeit/next-sass");
+const nextEnv = require("next-env");
+const dotenvLoad = require("dotenv-load");
 
 // Load .env file
 dotenvLoad();
 const withNextEnv = nextEnv({
-  publicPrefix: "EYET_"
+  publicPrefix: "EYET_",
 });
 
+// Workaround for https://github.com/zeit/next-plugins/issues/392
+function workaroundNextPluginsIssue392(config) {
+  console.warn(
+    "WARN: USING WORKAROUND FOR zeit/next-plugins/issues/392 (next.config.js)",
+  );
+  config.module.rules.forEach(rule => {
+    // Credit to github.com/KorenYA
+    // Source: https://github.com/KorenYa/react-course/pull/4
+    if (Array.isArray(rule.use)) {
+      rule.use.forEach(use => {
+        if (use.loader === "css-loader" && use.options) {
+          delete use.options.minimize;
+        }
+      });
+    }
+  });
+}
+
 module.exports = withNextEnv(
-  withTypescript(
-    withSass({
-    })
-  )
-)
+  withSass({
+    webpack: config => {
+      workaroundNextPluginsIssue392(config);
+      return config;
+    },
+  }),
+);
