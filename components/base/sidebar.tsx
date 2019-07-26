7a3b10c6b3a86789a09e7f08562e97e0361a94c3
John Su <git@tyncture.dev>
2019-07-27 00:58:30 +0100

    Make layout responsive

--- components/base/sidebar.tsx
+++ components/base/sidebar.tsx
@@ -1,33 +1,29 @@
 import React from "react";
 import "./sidebar.scss";
 
-class Sidebar extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div className="sidebar">
-        <header className="sidebar-header">
-          <input
-            type="text"
-            className="sidebar-header-search"
-            placeholder="Search here..."
-            spellCheck={false}
-          />
-        </header>
-        <div className="sidebar-content">
-          <nav className="sidebar-content-nav">
-            <div className="sidebar-content-nav-item sidebar-content-nav-item--active">Trending</div>
-            <div className="sidebar-content-nav-item">Most Popular</div>
-            <div className="sidebar-content-nav-item">Most Recent</div>
-          </nav>
-        </div>
-        <footer className="sidebar-footer" />
+function Sidebar(props) {
+  return (
+    <div className="sidebar">
+      <header className="sidebar-header">
+        <input
+          type="text"
+          className="sidebar-header-search"
+          placeholder="Search here..."
+          spellCheck={false}
+        />
+      </header>
+      <div className="sidebar-content">
+        <nav className="sidebar-content-nav">
+          <div className="sidebar-content-nav-item sidebar-content-nav-item--active">
+            Trending
+          </div>
+          <div className="sidebar-content-nav-item">Most Popular</div>
+          <div className="sidebar-content-nav-item">Most Recent</div>
+        </nav>
       </div>
-    );
-  }
+      <footer className="sidebar-footer" />
+    </div>
+  );
 }
 
 export default Sidebar;
