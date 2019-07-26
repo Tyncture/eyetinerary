eb76d5af4cf646a33b015af93fa51298271e1dc6
John Su <git@tyncture.dev>
2019-07-26 04:45:27 +0100

    Begin Itinerary page rewrite

--- pages/itinerary/[id].tsx
+++ pages/itinerary/[id].tsx
@@ -1,115 +1,71 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import BaseContainer from "../../components/base/baseContainer";
 import Sidebar from "../../components/base/sidebar";
 import Main from "../../components/base/main";
-import ItineraryOverview from "../../components/itinerary/views/itineraryOverview/itineraryOverview";
-import ItineraryPage from "../../components/itinerary/views/itineraryPage/itineraryPage";
-import { IItinerary } from "../../components/itinerary/common/types";
-import { getItinerary } from "../../common/requests";
-import validator from "validator";
-import ItineraryHeader from "../../components/itinerary/base/itineraryHeader";
+import { apiGet } from "../../common/requests";
+import Head from "next/head";
+import { connect } from "react-redux";
+import { IUser } from "../../store/user/types";
+import { ApiError } from "../../common/apiError";
+
+interface IItinerary {
+  title: string;
+  description: string;
+}
 
 interface IProps {
   query: {
     id: string;
-    page?: string;
-    item?: string;
   };
+  itinerary?: IItinerary;
+  user: IUser;
 }
 
-interface IState {
-  itinerary: IItinerary;
-  apiErrorCode: number;
+function Itinerary(props: IProps) {
+  const [itinerary, setItinerary] = useState<IItinerary>(props.itinerary);
+  const [apiError, setApiError] = useState<string>();
+
+  const retrieveData = async () => {
+    const bearerToken = props.user.token ? props.user.token : null;
+    const response = await apiGet(`/itinerary/${props.query.id}`, bearerToken);
+    if (response.success) {
+      setItinerary(response.body);
+    } else {
+      const error = new ApiError(response.statusCode);
+      setApiError(error.message);
+    }
+  };
+
+  useEffect(() => {
+    if (!itinerary) {
+      retrieveData();
+    }
+  });
+
+  const pageTitle = `${itinerary ? `${itinerary.title} - ` : ""}Eyetinerary`;
+
+  return (
+    <BaseContainer>
+      <Head>
+        <title>{pageTitle}</title>
+      </Head>
+      <Sidebar />
+      <Main>{JSON.stringify(itinerary)}</Main>
+    </BaseContainer>
+  );
 }
 
-const initialState: IState = {
-  itinerary: null,
-  apiErrorCode: null
+Itinerary.getInitialProps = async ({ query }) => {
+  const props = { query, itinerary: null };
+  const response = await apiGet(`/itinerary/${query.id}`);
+  if (response.success) {
+    props.itinerary = response.body;
+  }
+  return props;
 };
 
-class Itinerary extends React.Component<IProps, IState> {
-  static getInitialProps({ query }) {
-    return { query };
-  }
+const mapStateToProps = state => ({
+  user: state.user,
+});
 
-  constructor(props: IProps) {
-    super(props);
-    this.state = initialState;
-  }
-
-  async componentDidMount() {
-    await this.bootstrapState();
-  }
-
-  async componentDidUpdate(prevProps: IProps) {
-    if (this.props.query.id !== prevProps.query.id) {
-      const callback = () => this.bootstrapState();
-      this.setState({ itinerary: null, apiErrorCode: null }, callback);
-    }
-  }
-
-  async bootstrapState() {
-    if (this.propsAcceptable()) {
-      const response = await getItinerary(Number(this.props.query.id));
-      if (response.success) {
-        this.setState({ itinerary: response.body });
-      } else {
-        this.setState({ apiErrorCode: response.statusCode });
-      }
-    }
-  }
-
-  propsAcceptable(): boolean {
-    const itineraryIdAcceptable = this.props.query.id
-      ? validator.isNumeric(this.props.query.id)
-      : true;
-    const pageNumberAcceptable = this.props.query.page
-      ? validator.isNumeric(this.props.query.page)
-      : true;
-    const itemNumberAcceptable = this.props.query.item
-      ? validator.isNumeric(this.props.query.item)
-      : true;
-    return (
-      itineraryIdAcceptable && pageNumberAcceptable && itemNumberAcceptable
-    );
-  }
-
-  render() {
-    return (
-      <BaseContainer>
-        <Sidebar />
-        <Main>
-          {this.propsAcceptable() && (
-            <div>
-              {this.state.itinerary && (
-                <div>
-                  <ItineraryHeader
-                    title={this.state.itinerary.title}
-                    description="Testing React Frontend"
-                    location="Chiang Mai"
-                    countryCode="Thailand"
-                    className="itinerary-header pb-5"
-                  />
-                  {/* Dynamic URL routing /itinerary/:id/:page for Next.js */}
-                  {/* /itinerary/:id */
-                  !this.props.query.page && (
-                    <ItineraryOverview id={Number(this.props.query.id)} />
-                  )}
-                  {/* /itinerary/:id/:page */
-                  this.props.query.page && !this.props.query.item && (
-                    <ItineraryPage
-                      itineraryId={Number(this.props.query.id)}
-                      pageNumber={Number(this.props.query.page)}
-                    />
-                  )}
-                </div>
-              )}
-            </div>
-          )}
-        </Main>
-      </BaseContainer>
-    );
-  }
-}
-
-export default Itinerary;
+export default connect(mapStateToProps)(Itinerary);
