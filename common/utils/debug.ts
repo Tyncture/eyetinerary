a0cc35a13bb7e9d49d3780b884af7bc367f56659
John Su <git@tyncture.dev>
2019-07-27 15:09:15 +0100

    Add debug logger for SSR only

--- common/utils/debug.ts
+++ common/utils/debug.ts
@@ -1,5 +1,12 @@
-export function debug(message: string) {
+export function debug(message: any) {
    if (process.env.NODE_ENV !== "production") {
      console.log(message);
    }
+}
+
+export function debugSSR(message: any) {
+  const isSeverSideRenderer = typeof process.browser === "undefined";
+  if (isSeverSideRenderer) {
+    debug(message);
+  }
 }
