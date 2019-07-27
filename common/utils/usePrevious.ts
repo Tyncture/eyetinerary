f680f45b4a83ef9000337770ceca9739c5f45c42
John Su <git@tyncture.dev>
2019-07-27 06:06:49 +0100

    Use generics in usePrevious

--- common/utils/usePrevious.ts
+++ common/utils/usePrevious.ts
@@ -1,9 +1,9 @@
 import { useEffect, useRef } from "react";
 
-export function usePrevious(value) {
+export function usePrevious<T>(value: T): T {
   // Track previous props/state value
   // Ref: https://usehooks.com/usePrevious/
-  const ref = useRef();
+  const ref = useRef<T>();
   useEffect(() => {
     ref.current = value;
   }, [value]);
