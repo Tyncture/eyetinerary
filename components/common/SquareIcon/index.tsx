77f6b60db79de3f22b8ede0349218d3366bcbace
John Su <git@tyncture.dev>
2019-07-29 15:36:45 +0100

    Create common SquareIcon component

---
+++ components/common/SquareIcon/index.tsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+interface IProps {
+  children: any;
+  cssPadding?: number;
+  cssColor?: string;
+}
+
+function SquareIcon(props: IProps) {
+  const Component = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: ${props.cssPadding};
+    background: ${props.cssColor};
+    border-radius: 0.375rem;
+  `;
+  return <Component>{props.children}</Component>;
+}
+
+SquareIcon.defaultProps = {
+  cssPadding: "0.5rem",
+  cssColor: "#F6F7F9",
+};
+
+export default SquareIcon;
