5ae1265a0d3859332d3127b7990d40556992c412
John Su <git@tyncture.dev>
2019-08-16 17:53:47 +0100

    Modal structure and props

--- components/modal/index.tsx
+++ components/modal/index.tsx
@@ -1,12 +1,20 @@
+import "./index.scss";
+
 interface IProps {
-  children?: Array<JSX.Element | React.Component>;
+  children?:
+    | Array<JSX.Element | React.Component>
+    | JSX.Element
+    | React.Component;
+  show: boolean;
 }
 
 function Modal(props: IProps) {
   return (
-    <div className="modal">
-      <div className="modal__inner">{...props.children}</div>
-    </div>
+    props.show && (
+      <div className="modal">
+        <div className="modal__inner">{props.children}</div>
+      </div>
+    )
   );
 }
 
