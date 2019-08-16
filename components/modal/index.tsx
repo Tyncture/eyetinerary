26fa7d09c41f92f46fbed0031a8f44ae5d07a98f
John Su <git@tyncture.dev>
2019-08-17 00:44:19 +0100

    Implement modal styles

--- components/modal/index.tsx
+++ components/modal/index.tsx
@@ -6,13 +6,21 @@ interface IProps {
     | JSX.Element
     | React.Component;
   show: boolean;
+  title?: string;
 }
 
 function Modal(props: IProps) {
   return (
     props.show && (
       <div className="modal">
-        <div className="modal__inner">{props.children}</div>
+        <div className="modal__inner">
+          {props.title && (
+            <header className="modal__header">
+              <h1 className="modal__title">{props.title}</h1>
+            </header>
+          )}
+          {props.children}
+        </div>
       </div>
     )
   );
